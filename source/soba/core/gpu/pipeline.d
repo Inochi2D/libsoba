60759c4f029531853bf099660b5a56048a349f95
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-03-10 05:53:37 +0100

    WIP Textures in pipelines

--- source/soba/core/gpu/pipeline.d
+++ source/soba/core/gpu/pipeline.d
@@ -2,6 +2,7 @@ module soba.core.gpu.pipeline;
 import soba.core.gpu;
 import bindbc.wgpu;
 import std.string;
+import std.exception;
 
 /**
     Describes the kind of topology of the mesh being rendered
@@ -99,8 +100,33 @@ enum SbGFXWindingOrder {
     CW
 }
 
+
 class SbGFXPipeline {
 private:
+    struct SbGFXPipelineGroup {
+        uint type;
+        int bindloc;
+        union {
+            /// Type 0
+            SbGFXBufferBaseI buffer;
+
+            /// Type 1
+            SbGFXTextureView texture;
+        }
+
+        this(int bindloc, SbGFXBufferBaseI buffer) {
+            this.type = 0;
+            this.bindloc = bindloc;
+            this.buffer = buffer;
+        }
+
+        this(int bindloc, SbGFXTextureView texture) {
+            this.type = 1;
+            this.bindloc = bindloc;
+            this.texture = texture;
+        }
+    }
+
     const(char)* name;
     SbGFXContext ctx;
 
@@ -114,6 +140,7 @@ private:
     SbGFXWindingOrder winding = SbGFXWindingOrder.CCW;
     WGPURenderPipelineDescriptor desc;
     WGPURenderPipeline pipeline;
+    WGPUBindGroup bgroup;
 
     // Pipeline data
     const(char)* fragMain;
@@ -122,6 +149,9 @@ private:
     SbGFXBufferBaseI[] buffers;
     SbGFXTexture[] textures;
     SbGFXTextureView[] targets;
+    SbGFXPipelineGroup[int] bindgroups;
+    WGPUBindGroupLayoutEntry[] bglentries;
+    WGPUBindGroupEntry[] bgentries;
 
     WGPUVertexBufferLayout[] generateVertexBufferBindings() {
         WGPUVertexBufferLayout[] layouts;
@@ -198,6 +228,143 @@ private:
             states.ptr
         );
 
+        bglentries.length = 0;
+        bgentries.length = 0;
+        foreach(group; bindgroups) {
+            switch(group.type) {
+                case 0:
+                    bgentries ~= WGPUBindGroupEntry(
+                        null,
+                        group.bindloc,
+                        group.buffer.getHandle(),
+                        0,
+                        group.buffer.getSize(),
+                        null,
+                        null
+                    );
+                    bglentries ~= WGPUBindGroupLayoutEntry(
+                        null,
+                        group.bindloc,
+                        WGPUShaderStage.Fragment,
+                        WGPUBufferBindingLayout(
+                            null,
+                            WGPUBufferBindingType.Uniform,
+                            false,
+                            group.buffer.getSize()
+                        ),
+                        WGPUSamplerBindingLayout(
+                            null,
+                            WGPUSamplerBindingType.Undefined
+                        ),
+                        WGPUTextureBindingLayout(
+                            null,
+                            WGPUTextureSampleType.Undefined,
+                            WGPUTextureViewDimension.Undefined,
+                            false
+                        ),
+                        WGPUStorageTextureBindingLayout(
+                            null,
+                            WGPUStorageTextureAccess.Undefined,
+                            WGPUTextureFormat.Undefined,
+                            WGPUTextureViewDimension.Undefined,
+                        )
+                    );
+                    break;
+                case 1:
+                    bgentries ~= WGPUBindGroupEntry(
+                        null,
+                        group.bindloc,
+                        null,
+                        0,
+                        0,
+                        null,
+                        group.texture.currentView()
+                    );
+                    bgentries ~= WGPUBindGroupEntry(
+                        null,
+                        group.bindloc+1,
+                        null,
+                        0,
+                        0,
+                        group.texture.getNativeSampler(),
+                        null
+                    );
+                    bglentries ~= WGPUBindGroupLayoutEntry(
+                        null,
+                        group.bindloc,
+                        WGPUShaderStage.Fragment,
+                        WGPUBufferBindingLayout(
+                            null,
+                            WGPUBufferBindingType.Undefined,
+                            false,
+                            0
+                        ),
+                        WGPUSamplerBindingLayout(
+                            null,
+                            WGPUSamplerBindingType.Undefined
+                        ),
+                        WGPUTextureBindingLayout(
+                            null,
+                            WGPUTextureSampleType.Float,
+                            WGPUTextureViewDimension.D2,
+                            false
+                        ),
+                        WGPUStorageTextureBindingLayout(
+                            null,
+                            WGPUStorageTextureAccess.Undefined,
+                            WGPUTextureFormat.Undefined,
+                            WGPUTextureViewDimension.Undefined,
+                        )
+                    );
+                    bglentries ~= WGPUBindGroupLayoutEntry(
+                        null,
+                        group.bindloc+1,
+                        WGPUShaderStage.Fragment,
+                        WGPUBufferBindingLayout(
+                            null,
+                            WGPUBufferBindingType.Undefined,
+                            false,
+                            0
+                        ),
+                        WGPUSamplerBindingLayout(
+                            null,
+                            WGPUSamplerBindingType.Filtering
+                        ),
+                        WGPUTextureBindingLayout(
+                            null,
+                            WGPUTextureSampleType.Undefined,
+                            WGPUTextureViewDimension.Undefined,
+                            false
+                        ),
+                        WGPUStorageTextureBindingLayout(
+                            null,
+                            WGPUStorageTextureAccess.Undefined,
+                            WGPUTextureFormat.Undefined,
+                            WGPUTextureViewDimension.Undefined,
+                        )
+                    );
+                    break;
+                default: break;
+            }
+        }
+
+        WGPUBindGroupLayoutDescriptor bgroupdesc;
+        bgroupdesc.entryCount = cast(uint)bglentries.length;
+        bgroupdesc.entries = bglentries.ptr;
+        WGPUBindGroupLayout bgroupLayout = wgpuDeviceCreateBindGroupLayout(ctx.getDevice(), &bgroupdesc);
+
+        WGPUBindGroupDescriptor bgdesc;
+        bgdesc.entries = bgentries.ptr;
+        bgdesc.entryCount = cast(uint)bgentries.length;
+        bgdesc.layout = bgroupLayout;
+
+        bgroup = wgpuDeviceCreateBindGroup(ctx.getDevice(), &bgdesc);
+
+        WGPUPipelineLayoutDescriptor pldesc;
+        pldesc.bindGroupLayoutCount = 1;
+        pldesc.bindGroupLayouts = &bgroupLayout;
+
+        desc.layout = wgpuDeviceCreatePipelineLayout(ctx.getDevice, &pldesc);
         pipeline = wgpuDeviceCreateRenderPipeline(ctx.getDevice(), &desc);
     }
 
@@ -221,17 +388,26 @@ public:
     }
 
     /**
-        Adds a texture to the pipeline
+        Adds a buffer to the pipeline
     */
-    void addBuffer(SbGFXBufferBaseI buffer) {
+    void addVertexBuffer(SbGFXBufferBaseI buffer) {
+        enforce(buffer.getType() == SbGFXBufferType.Vertex, "Invalid buffer type!");
         this.buffers ~= buffer;
+    }
+
+    /**
+        Adds a uniform buffer to the pipeline
+    */
+    void setUniformBuffer(SbGFXBufferBaseI buffer, int bindloc) {
+        enforce(buffer.getType() == SbGFXBufferType.Uniform, "Invalid buffer type!");
+        this.bindgroups[bindloc] = SbGFXPipelineGroup(bindloc, buffer);
     }
 
     /**
         Adds a texture to the pipeline
     */
-    void addTexture(SbGFXTexture texture) {
-        this.shader = shader;
+    void setTexture(SbGFXTexture texture, int bindloc) {
+        this.bindgroups[bindloc] = SbGFXPipelineGroup(bindloc, texture);
     }
 
     /**
@@ -320,5 +496,11 @@ public:
     final
     WGPURenderPipeline getHandle() {
         return pipeline;
+    }
+
+    /// Returns the underlying WGPU handle
+    final
+    WGPUBindGroup getBindGroupHandle() {
+        return bgroup;
     }
 }
