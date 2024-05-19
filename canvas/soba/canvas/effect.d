5a0bd5316a5599ef50359dae7ae1b696f92b4f33
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-05-19 16:47:31 +0200

    Improve Effect interface

--- canvas/soba/canvas/effect.d
+++ canvas/soba/canvas/effect.d
@@ -9,6 +9,7 @@ module soba.canvas.effect;
 import soba.canvas.canvas;
 import numem.all;
 import inmath;
+import inteli.emmintrin;
 
 /**
     A graphical effect applied to a surface
@@ -17,6 +18,59 @@ abstract
 class SbEffect {
 nothrow @nogc:
 private:
+protected:
+
+    /**
+        Gets a pixel as a 128 bit SSE value
+    */
+    pragma(inline, true)
+    __m128i getPixelSSE(ubyte* data, vec2i pos, ptrdiff_t alignment, ptrdiff_t stride) {
+
+        // TODO: use _mm_load?
+        ptrdiff_t offset = (pos.y*stride)+(pos.x*alignment);
+        ubyte[] pixels = data[offset..offset+alignment];
+        int[4] values;
+
+        foreach(i; 0..alignment) {
+            values[i] = pixels[i];
+        }
+
+        return _mm_setr_epi32(values[0], values[1], values[2], values[3]);
+    }
+
+    /**
+        Sets a pixel as a 128 bit SSE value
+    */
+    pragma(inline, true)
+    void setPixelSSE(ubyte* data, vec2i pos, __m128i value, ptrdiff_t alignment, ptrdiff_t stride) {
+
+        size_t offset = (pos.y*stride)+(pos.x*alignment);
+        foreach(i; 0..alignment) {
+            data[offset+i] = cast(ubyte)value.array[i];
+        }
+
+        // Flush cache
+        _mm_clflush(data);
+    }
+
+    /**
+        Gets a pixel as a byte slice
+    */
+    ubyte[] getPixel(ubyte* data, vec2i pos, ptrdiff_t alignment, ptrdiff_t stride) {
+        ptrdiff_t offset = (pos.y*stride)+(pos.x*alignment);
+        return data[offset..offset+alignment];
+    }
+
+    /**
+        Sets a pixel as a byte slice
+    */
+    void setPixel(ubyte* data, vec2i pos, ubyte[] value, ptrdiff_t alignment, ptrdiff_t stride) {
+
+        size_t offset = (pos.y*stride)+(pos.x*alignment);
+        foreach(i; 0..min(alignment, value.length)) {
+            data[offset+i] = value[i];
+        }
+    }
 public:
     abstract void apply(ref SbCanvas canvas, recti clipArea=recti(-1, -1, -1, -1));
 }
--- canvas/soba/canvas/effects/blur.d
+++ canvas/soba/canvas/effects/blur.d
@@ -23,100 +23,130 @@ nothrow @nogc:
 private:
     uint blurSigma;
 
-    static if (!SSESizedVectorsAreEmulated) {
+    // SIMD
+    void blurSSE(bool direction)(ref SbCanvas canvas, recti area)  {
+        ptrdiff_t alignment = canvas.getAlign();
+        ptrdiff_t stride = canvas.getStride();
+        ubyte* data = canvas.lock();
 
-        pragma(inline, true)
-        __m128i getPixel(ubyte* data, vec2i pos, ptrdiff_t alignment, ptrdiff_t stride) {
+        foreach(y; area.top..area.bottom) {
+            foreach(x; area.left..area.right) {
+                
+                int sumCount = 1;
+                __m128i sum = getPixelSSE(data, vec2i(x, y), alignment, stride);
 
-            // TODO: use _mm_load?
-            ptrdiff_t offset = (pos.y*stride)+(pos.x*alignment);
-            ubyte[] pixels = data[offset..offset+alignment];
-            int[4] values;
+                foreach(i; 1..blurSigma) {
+                    __m128i pixp = _mm_set1_epi32(0);
 
-            foreach(i; 0..alignment) {
-                values[i] = pixels[i];
-            }
+                    static if (direction) {
 
-            return _mm_setr_epi32(values[0], values[1], values[2], values[3]);
-        }
-
-        pragma(inline, true)
-        void setPixel(ubyte* data, vec2i pos, __m128i value, ptrdiff_t alignment, ptrdiff_t stride) {
-
-            size_t offset = (pos.y*stride)+(pos.x*alignment);
-            foreach(i; 0..alignment) {
-                data[offset+i] = cast(ubyte)value.array[i];
-            }
-
-            // Flush cache
-            _mm_clflush(data);
-        }
-
-        // SIMD
-        void blur(bool direction)(ref SbCanvas canvas, recti area)  {
-            ptrdiff_t alignment = canvas.getAlign();
-            ptrdiff_t stride = canvas.getStride();
-            ubyte* data = canvas.lock();
-
-            foreach(y; area.top..area.bottom) {
-                foreach(x; area.left..area.right) {
-                    
-                    int sumCount = 1;
-                    __m128i sum = getPixel(data, vec2i(x, y), alignment, stride);
-
-                    foreach(i; 1..blurSigma) {
-                        __m128i pixp = _mm_set1_epi32(0);
-
-                        static if (direction) {
-
-                            ptrdiff_t up = cast(ptrdiff_t)y-cast(ptrdiff_t)i;
-                            ptrdiff_t down = cast(ptrdiff_t)y+cast(ptrdiff_t)i;
-                            if (up >= area.top) {
-                                pixp = _mm_add_epi32(pixp, getPixel(data, vec2i(x, cast(int)up), alignment, stride));
-                                sumCount++;
-                            }
-
-                            if (down <= area.bottom) {
-                                pixp = _mm_add_epi32(pixp, getPixel(data, vec2i(x, cast(int)down), alignment, stride));
-                                sumCount++;
-                            }
-                        } else {
-                            
-                            ptrdiff_t left = cast(ptrdiff_t)x-cast(ptrdiff_t)i;
-                            ptrdiff_t right = cast(ptrdiff_t)x+cast(ptrdiff_t)i;
-                            if (left >= area.left) {
-                                pixp = _mm_add_epi32(pixp, getPixel(data, vec2i(cast(int)left, y), alignment, stride));
-                                sumCount++;
-                            }
-
-                            if (right <= area.right) {
-                                pixp = _mm_add_epi32(pixp, getPixel(data, vec2i(cast(int)right, y), alignment, stride));
-                                sumCount++;
-                            }  
+                        ptrdiff_t up = cast(ptrdiff_t)y-cast(ptrdiff_t)i;
+                        ptrdiff_t down = cast(ptrdiff_t)y+cast(ptrdiff_t)i;
+                        if (up >= area.top) {
+                            pixp = _mm_add_epi32(pixp, getPixelSSE(data, vec2i(x, cast(int)up), alignment, stride));
+                            sumCount++;
                         }
 
-                        // Set sum
-                        sum = _mm_add_epi32(sum, pixp);
+                        if (down <= area.bottom) {
+                            pixp = _mm_add_epi32(pixp, getPixelSSE(data, vec2i(x, cast(int)down), alignment, stride));
+                            sumCount++;
+                        }
+                    } else {
+                        
+                        ptrdiff_t left = cast(ptrdiff_t)x-cast(ptrdiff_t)i;
+                        ptrdiff_t right = cast(ptrdiff_t)x+cast(ptrdiff_t)i;
+                        if (left >= area.left) {
+                            pixp = _mm_add_epi32(pixp, getPixelSSE(data, vec2i(cast(int)left, y), alignment, stride));
+                            sumCount++;
+                        }
+
+                        if (right <= area.right) {
+                            pixp = _mm_add_epi32(pixp, getPixelSSE(data, vec2i(cast(int)right, y), alignment, stride));
+                            sumCount++;
+                        }  
                     }
 
-                    __m128 fsum = _mm_castsi128_ps(sum);
-                    __m128 fcount = _mm_set1_ps(cast(float)sumCount);
-                    __m128 result = _mm_div_ps(fsum, fcount);
-                    setPixel(data, vec2i(x, y), _mm_castps_si128(result), alignment, stride);
+                    // Set sum
+                    sum = _mm_add_epi32(sum, pixp);
                 }
+
+                __m128 fsum = _mm_castsi128_ps(sum);
+                __m128 fcount = _mm_set1_ps(cast(float)sumCount);
+                __m128 result = _mm_div_ps(fsum, fcount);
+                setPixelSSE(data, vec2i(x, y), _mm_castps_si128(result), alignment, stride);
             }
-
-            canvas.unlock();
         }
-    } else {
 
-        // NO SIMD
-        void blur(ref SbCanvas canvas, recti area) {
-
-        }
+        canvas.unlock();
     }
 
+    // NO SIMD
+    void blur(bool direction)(ref SbCanvas canvas, recti area) {
+        ptrdiff_t alignment = canvas.getAlign();
+        ptrdiff_t stride = canvas.getStride();
+        ubyte* data = canvas.lock();
 
+        foreach(y; area.top..area.bottom) {
+            foreach(x; area.left..area.right) {
+
+                // Largest pixel size possible is 4-byte aligned.
+                int sumCount = 1;
+                int[4] sum;
+                ubyte[4] acc;
+
+                foreach(i, pix; getPixel(data, vec2i(x, y), alignment, stride)) {
+                    sum[i] = cast(int)pix;
+                }
+
+                foreach(i; 1..blurSigma) {
+                    int[4] pixp;
+
+                    static if (direction) {
+
+                        ptrdiff_t up = cast(ptrdiff_t)y-cast(ptrdiff_t)i;
+                        ptrdiff_t down = cast(ptrdiff_t)y+cast(ptrdiff_t)i;
+                        if (up >= area.top) {
+                            auto pixel = getPixel(data, vec2i(x, cast(int)up), alignment, stride);
+                            foreach(ix; 0..alignment) pixp[ix] += pixel[ix];
+                            sumCount++;
+                        }
+
+                        if (down <= area.bottom) {
+                            auto pixel = getPixel(data, vec2i(x, cast(int)down), alignment, stride);
+                            foreach(ix; 0..alignment) pixp[ix] += pixel[ix];
+                            sumCount++;
+                        }
+                    } else {
+                        
+                        ptrdiff_t left = cast(ptrdiff_t)x-cast(ptrdiff_t)i;
+                        ptrdiff_t right = cast(ptrdiff_t)x+cast(ptrdiff_t)i;
+                        if (left >= area.left) {
+                            auto pixel = getPixel(data, vec2i(cast(int)left, y), alignment, stride);
+                            foreach(ix; 0..alignment) pixp[ix] += pixel[ix];
+                            sumCount++;
+                        }
+
+                        if (right <= area.right) {
+                            auto pixel = getPixel(data, vec2i(cast(int)right, y), alignment, stride);
+                            foreach(ix; 0..alignment) pixp[ix] += pixel[ix];
+                            sumCount++;
+                        }  
+                    }
+
+                    // Set sum
+                    sum[0..4] += pixp[0..4];
+                }
+
+                sum[0..4] /= sumCount;
+                foreach(i; 0..sum.length) {
+                    acc[i] = cast(ubyte)sum[i];
+                }
+
+                setPixel(data, vec2i(x, y), acc[0..alignment], alignment, stride);
+            }
+        }
+        canvas.unlock();
+    }
 
 public:
     this(uint blurSigma) {
@@ -145,7 +175,12 @@ public:
 
         // Make sure we don't try reading pixels in invalid memory
         clipArea.clip(recti(0, 0, canvas.getWidth(), canvas.getHeight()));
-        this.blur!true(canvas, clipArea);
-        this.blur!false(canvas, clipArea);
+        static if (!SSESizedVectorsAreEmulated) {
+            this.blurSSE!true(canvas, clipArea);
+            this.blurSSE!false(canvas, clipArea);
+        } else {
+            this.blur!true(canvas, clipArea);
+            this.blur!false(canvas, clipArea);
+        }
     }
 }
