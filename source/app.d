4832ceeb07750ad7976013d22b5d6e37870f2527
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-08-18 13:45:17 +0200

    Make sure silly unittests don't conflict with demo mode

--- source/app.d
+++ source/app.d
@@ -1,59 +1,65 @@
 module app;
-import soba;
-import numem.all;
-import numem.conv;
 
-import soba.ssk;
-import soba.sio;
-import soba.canvas;
+// Demo mode is in a version statement to prevent it interfering with
+// Unittests run by Silly, this will later be removed however in favour
+// Of proper example apps.
+version(DemoMode) {
+    import soba;
+    import numem.all;
+    import numem.conv;
 
-import soba.widgets;
+    import soba.ssk;
+    import soba.sio;
+    import soba.canvas;
 
-void main() {
-    sbInit();
-    
-    SbAppInfo info;
-    info.name = "My Cool App";
-    info.version_ = "1.0.0";
+    import soba.widgets;
 
-    MyDrawingWidget widget = nogc_new!MyDrawingWidget();
+    void main() {
+        sbInit();
+        
+        SbAppInfo info;
+        info.name = "My Cool App";
+        info.version_ = "1.0.0";
 
-    SbWindow window = nogc_new!SbWindow(info.name, 640, 480);
-    window.setChild(widget);
-    SbApplication app = nogc_new!SbApplication(info);
-    app.setRootWindow(window);
+        MyDrawingWidget widget = nogc_new!MyDrawingWidget();
 
-    app.run();
-}
+        SbWindow window = nogc_new!SbWindow(info.name, 640, 480);
+        window.setChild(widget);
+        SbApplication app = nogc_new!SbApplication(info);
+        app.setRootWindow(window);
 
-
-class MyDrawingWidget : SbDrawingWidget {
-@nogc:
-private:
-    SbFontFace face;
-    SbFont font;
-
-protected:
-    override
-    void onDraw(SbContext ctx) {
-        ctx.setSource(vec4(0, 0, 0, 1));
-        ctx.clearAll();
-
-        recti bounds = this.getBounds().expanded(vec2i(-32, -32));
-
-        ctx.setSource(vec4(0.5, 0.5, 0.5, 1));
-        ctx.rect(rect(bounds.x, bounds.y, bounds.width, bounds.height));
-        ctx.fill();
-
-        ctx.setSource(vec4(1, 1, 1, 1));
-        vec2 cont = ctx.fillText(font, this.getBounds().width.toString(), vec2(32, 32));
-        cont = ctx.fillText(font, nstring("x"), cont);
-        cont = ctx.fillText(font, this.getBounds().width.toString(), cont);
+        app.run();
     }
 
-public:
-    this() {
-        face = nogc_new!SbFontFace(nstring("noto.ttf"), 0);
-        font = nogc_new!SbFont(face, 24);
+
+    class MyDrawingWidget : SbDrawingWidget {
+    @nogc:
+    private:
+        SbFontFace face;
+        SbFont font;
+
+    protected:
+        override
+        void onDraw(SbContext ctx) {
+            ctx.setSource(vec4(0, 0, 0, 1));
+            ctx.clearAll();
+
+            recti bounds = this.getBounds().expanded(vec2i(-32, -32));
+
+            ctx.setSource(vec4(0.5, 0.5, 0.5, 1));
+            ctx.rect(rect(bounds.x, bounds.y, bounds.width, bounds.height));
+            ctx.fill();
+
+            ctx.setSource(vec4(1, 1, 1, 1));
+            vec2 cont = ctx.fillText(font, this.getBounds().width.toString(), vec2(32, 32));
+            cont = ctx.fillText(font, nstring("x"), cont);
+            cont = ctx.fillText(font, this.getBounds().width.toString(), cont);
+        }
+
+    public:
+        this() {
+            face = nogc_new!SbFontFace(nstring("noto.ttf"), 0);
+            font = nogc_new!SbFont(face, 24);
+        }
     }
 }
